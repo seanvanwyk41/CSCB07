94fafc275111b4d63ec618fb9763a29a66e0b6c1
seanvanwyk41 <seanvanwyk41@gmail.com>
2017-09-07 20:32:40 -0400

    First commit added a comment to main class [CSCB07_Practice.java].

---
+++ CSCB07_Practice/src/cscb07_practice/CSCB07_Practice.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cscb07_practice;
+
+/**
+ *
+ * @author seanv
+ */
+public class CSCB07_Practice {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    // This is my first line of code for the repo here's hoping it does well
+  }
+  
+}
